2c9a188749956c9d7819bb3ae69c45556697c2e6
champacademy2u-oss <champacademy2u@gmail.com>
2026-08-09 17:09:37 +0800

    Refresh cached lead email assets

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "lead-center-v7";
+const CACHE_NAME = "lead-center-v8";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js",
+  "./app.js?v=20260809-lead-email",
   "./config.js",
   "./manifest.json",
   "./zoom.html",
