144e9cd1a37e755e6975d91813c3cef04a657556
champacademy2u-oss <champacademy2u@gmail.com>
2026-08-10 20:17:33 +0800

    Fix campaign API routing

--- api/email-campaigns.js
+++ api/email-campaigns.js
@@ -1,8 +1,10 @@
-import { handleEmailCampaignRequest, handleEmailUnsubscribeRequest } from '../lib/email-campaign-api.js';
+import { handleEmailCampaignRequest } from '../lib/email-campaign-api.js';
+import handleEmailTrackingRequest from './email-track.js';
+import handleEmailUnsubscribeRequest from './unsubscribe.js';
 
 export default async function handler(req, res) {
-  if (new URL(req.url, 'https://local').pathname.endsWith('/unsubscribe')) {
-    return handleEmailUnsubscribeRequest(req, res);
-  }
+  const pathname = new URL(req.url, 'https://local').pathname;
+  if (pathname.endsWith('/unsubscribe')) return handleEmailUnsubscribeRequest(req, res);
+  if (pathname.endsWith('/email-track')) return handleEmailTrackingRequest(req, res);
   return handleEmailCampaignRequest(req, res);
 }
